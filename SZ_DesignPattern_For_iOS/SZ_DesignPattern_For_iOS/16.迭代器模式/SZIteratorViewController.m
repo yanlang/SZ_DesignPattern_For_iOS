1926d8014ddd672436076eb58044e47594a4fa9d
shanzhu <lang.yan@hipac.cn>
2017-12-27 11:15:49 +0800

    尝试移除更改标志

---
+++ SZ_DesignPattern_For_iOS/SZ_DesignPattern_For_iOS/16.迭代器模式/SZIteratorViewController.m
@@ -0,0 +1,26 @@
+//
+//  SZIteratorViewController.m
+//  SZ_DesignPattern_For_iOS
+//
+//  Created by yanl on 2017/12/27.
+//  Copyright © 2017年 yanl. All rights reserved.
+//
+
+#import "SZIteratorViewController.h"
+
+@interface SZIteratorViewController ()
+
+@end
+
+@implementation SZIteratorViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    self.title = @"迭代器模式";
+    
+    self.lblTips.text = @"该模式在OC中，集合类已完美实现，该模式无代码！！！";
+}
+
+@end
